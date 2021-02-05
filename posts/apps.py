1b8a8fbbdf408d94d00948afc1440f6611988ded
Walther Smith <wsfrancootero@gmail.com>
2021-02-05 00:12:20 -0500

    se agrega ejemplo de post

--- posts/apps.py
+++ posts/apps.py
@@ -2,4 +2,13 @@ from django.apps import AppConfig
 
 
 class PostsConfig(AppConfig):
+    """post application settings"""
     name = 'posts'
+    verbose_name = 'Posts'
+
+
+
+
+"""
+https://docs.djangoproject.com/en/2.0/ref/applications/
+"""
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
 
-# Create your views here.
+#Django
+from django.http import HttpResponse
+#utilities
+from datetime import datetime
+
+posts = [
+    {
+        'name': 'Mont Blac',
+        'user': 'Yésica Cortés',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036',
+    },
+    {
+        'name': 'Via Láctea',
+        'user': 'C. Vander',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Thespianartist',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',
+    }
+]
+
+def list_posts(request0):
+    """list existings posts."""
+
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i> {timestamp} </i></small></p>
+            <figure><img src="{picture}"/></figure>
+        """.format(**post))
+
+    return HttpResponse('<br>'.join(content))
