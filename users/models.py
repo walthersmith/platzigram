d8f74696a84ba8c1567d93e3907f2226bd58d1a9
walthersmith <wsfrancootero@gmail.com>
2021-02-11 20:25:29 -0500

    se creo modelo  profile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
+"""Users Models."""
+
+#Django
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """ profile Model
+    Proxi model tat extends the base data with other 
+    information"""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(max_length=200, blank=True)
+    biograpy = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20,blank=True)
+
+    picture = models.ImageField(upload_to='users/pictures',blank=True,null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """return Username"""
+        return self.user
+
+
+
