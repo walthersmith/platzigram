b092fe443d131adc2b3ecea9e47e0f09f3685a63
walthersmith <wsfrancootero@gmail.com>
2021-02-21 13:43:43 -0500

    se agrega Middleware y formulario para actualizar perfil

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'platzigram.middleware.profileCompletionMiddleware',
 ]
 
 ROOT_URLCONF = 'platzigram.urls'
--- users/views.py
+++ users/views.py
@@ -12,6 +12,19 @@ from users.models import Profile
 from django.db.utils import IntegrityError
 
 
+
+def update_profile(request):
+    """Update a user's profile view"""
+    profile = request.user.profile
+
+    return render(request=request,
+        template_name='users/update_profile.html',
+        context={
+            'profile':profile,
+            'user':request.user
+        }
+    )
+
 def login_view(request):
     """Login View"""
     #import pdb; pdb.set_trace()
